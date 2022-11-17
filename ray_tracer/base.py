788d4e9664c3f8e8dd1a673beb2e3d8d12d9e1e1
S. Co1 <sco1.git@gmail.com>
2022-11-16 20:21:05 -0500

    Finish implementing initial Rayple arithmetic functions

--- ray_tracer/base.py
+++ ray_tracer/base.py
@@ -7,13 +7,28 @@ from enum import IntEnum
 NUMERIC_T = int | float
 
 
-class RaypleType(IntEnum):
+class RaypleType(IntEnum):  # noqa: D101
     VECTOR = 0
     POINT = 1
 
 
 @dataclass(frozen=True, slots=True)
 class Rayple:
+    """
+    The Ray Tracer's generic ordered list of (x,y,z) things.
+
+    The following operations are supported:
+        * Vector/Point addition
+            * Adding two points is undefined
+            * Scalar addition is undefined
+        * Vector/Point subtraction
+            * Subtracting a point from a vector is undefined
+            * Scalar subtraction is undefined
+        * Negation
+        * Scalar multiplication
+        * Scalar division
+    """
+
     x: NUMERIC_T
     y: NUMERIC_T
     z: NUMERIC_T
@@ -46,10 +61,59 @@ class Rayple:
             w=RaypleType(self.w + other.w),
         )
 
+    def __sub__(self, other: object) -> Rayple:
+        if not isinstance(other, Rayple):
+            return NotImplemented
+
+        if self.w == RaypleType.VECTOR and other.w == RaypleType.POINT:
+            raise ValueError("Cannot subtract a point from a vector.")
+
+        return Rayple(
+            x=self.x - other.x,
+            y=self.y - other.y,
+            z=self.z - other.z,
+            w=RaypleType(self.w - other.w),
+        )
+
+    def __neg__(self) -> Rayple:
+        return Rayple(
+            x=-self.x,
+            y=-self.y,
+            z=-self.z,
+            w=self.w,
+        )
+
+    def __mul__(self, other: object) -> Rayple:
+        if not isinstance(other, (int, float)):
+            return NotImplemented
+
+        return Rayple(
+            x=self.x * other,
+            y=self.y * other,
+            z=self.z * other,
+            w=self.w,
+        )
+
+    def __rmul__(self, other: object) -> Rayple:
+        return self * other
+
+    def __truediv__(self, other: object) -> Rayple:
+        if not isinstance(other, (int, float)):
+            return NotImplemented
+
+        return Rayple(
+            x=self.x / other,
+            y=self.y / other,
+            z=self.z / other,
+            w=self.w,
+        )
+
 
 def point(x: NUMERIC_T, y: NUMERIC_T, z: NUMERIC_T) -> Rayple:
+    """Shortcut for a point `Rayple` (`w = 1`)."""
     return Rayple(x, y, z, RaypleType.POINT)
 
 
 def vector(x: NUMERIC_T, y: NUMERIC_T, z: NUMERIC_T) -> Rayple:
+    """Shortcut for a vector `Rayple` (`w = 0`)."""
     return Rayple(x, y, z, RaypleType.VECTOR)
--- tests/test_rayple.py
+++ tests/test_rayple.py
@@ -2,6 +2,8 @@ import pytest
 
 from ray_tracer.base import Rayple, RaypleType, point, vector
 
+NUMERIC_T = int | float
+
 
 def test_rayple_components() -> None:
     rp = Rayple(4.3, -4.2, 3.1, RaypleType.POINT)
@@ -10,6 +12,24 @@ def test_rayple_components() -> None:
     assert rp.y == pytest.approx(-4.2)
     assert rp.z == pytest.approx(3.1)
     assert rp.w == RaypleType.POINT
+
+
+EQ_CASES = (
+    (point(1, 2, 3), point(1, 2, 3), True),
+    (point(1, 2, 3), point(1.0, 2.0, 3.0), True),
+    (vector(1, 2, 3), vector(1, 2, 3), True),
+    (vector(1, 2, 3), vector(1.0, 2.0, 3.0), True),
+    (point(1, 2, 3), point(-1.0, 2.0, 3.0), False),
+    (vector(1, 2, 3), vector(-1.0, 2.0, 3.0), False),
+    (point(1, 2, 3), vector(1, 2, 3), False),
+    (point(1, 2, 3), 5, False),
+    (vector(1, 2, 3), 5, False),
+)
+
+
+@pytest.mark.parametrize(("left", "right", "truth"), EQ_CASES)
+def test_rayple_eq(left: Rayple, right: Rayple | int, truth: bool) -> None:
+    assert (left == right) == truth
 
 
 def test_sum_point_vector() -> None:
@@ -33,6 +53,83 @@ def test_sum_point_point_raises() -> None:
     with pytest.raises(ValueError):
         p1 + p2
 
+
 def test_sum_rayple_nonrayple_raises() -> None:
     with pytest.raises(TypeError):
         point(0, 0, 0) + 5
+
+
+def test_diff_point_point() -> None:
+    p1 = point(3, 2, 1)
+    p2 = point(5, 6, 7)
+
+    assert (p1 - p2) == vector(-2, -4, -6)
+
+
+def test_diff_point_vector() -> None:
+    p = point(3, 2, 1)
+    v = vector(5, 6, 7)
+
+    assert (p - v) == point(-2, -4, -6)
+
+
+def test_diff_vector_vector() -> None:
+    v1 = vector(3, 2, 1)
+    v2 = vector(5, 6, 7)
+
+    assert (v1 - v2) == vector(-2, -4, -6)
+
+
+def test_diff_vector_point_raises() -> None:
+    p = point(3, 2, 1)
+    v = vector(5, 6, 7)
+
+    with pytest.raises(ValueError):
+        v - p
+
+
+def test_diff_rayple_nonrayple_raises() -> None:
+    with pytest.raises(TypeError):
+        point(0, 0, 0) - 5
+
+
+def test_negation() -> None:
+    p = point(1, -2, 3)
+    assert -p == point(-1, 2, -3)
+
+
+SCALAR_MUL_CASES = (
+    (point(1, -2, 3), 3.5, point(3.5, -7, 10.5)),
+    (3.5, point(1, -2, 3), point(3.5, -7, 10.5)),
+    (point(1, -2, 3), 3, point(3, -6, 9)),
+    (3, point(1, -2, 3), point(3, -6, 9)),
+)
+
+
+@pytest.mark.parametrize(("left", "right", "truth"), SCALAR_MUL_CASES)
+def test_scalar_multipliciation(
+    left: Rayple | NUMERIC_T, right: Rayple | NUMERIC_T, truth: Rayple
+) -> None:
+    assert (left * right) == truth
+
+
+def test_nonscalar_multiplication_raises() -> None:
+    with pytest.raises(TypeError):
+        point(1, 2, 3) * point(1, 2, 3)
+
+
+def test_scalar_division() -> None:
+    p = point(1, -2, 3)
+    assert p / 2 == point(0.5, -1, 1.5)
+
+
+def test_nonscalar_division_raises() -> None:
+    p = point(1, -2, 3)
+    with pytest.raises(TypeError):
+        p / p
+
+
+def test_scalar_rdivision_raises() -> None:
+    p = point(1, -2, 3)
+    with pytest.raises(TypeError):
+        2 / p  # type: ignore[operator]
