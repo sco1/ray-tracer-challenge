a44a626508b616fb01f72ff1e4705cd1eff3fc97
S. Co1 <sco1.git@gmail.com>
2022-11-19 10:18:52 -0500

    Add Canvas to PPM writer

--- ray_tracer/canvas.py
+++ ray_tracer/canvas.py
@@ -1,3 +1,4 @@
+import textwrap
 from pathlib import Path
 
 import numpy as np
@@ -15,12 +16,69 @@ class Canvas:  # noqa: D101
         self._pixels = np.zeros(shape=(width, height, 3))
 
     def pixel_at(self, x: int, y: int) -> Rayple:
+        """Return the Color value for the queried pixel."""
         return color(*self._pixels[x, y, :])
 
     def write_pixel(self, x: int, y: int, color: Rayple) -> None:
+        """Map the provided Color value to the specified pixel location."""
         if color.w != RaypleType.COLOR:
             raise ValueError(f"Expected Color Rayple. Received: {type(color.w)}")
 
         self._pixels[x, y, :] = (*color,)
 
-    ...
+    def to_ppm(self, out_filepath: Path) -> None:
+        """
+        Output the current canvas as a Portable Pixmap (PPM).
+
+        See: https://en.wikipedia.org/wiki/Netpbm for more info on the file format.
+        """
+        full_text = (
+            f"{_build_ppm_header(self.width, self.height)}\n{_pixels_to_ppm(self._pixels)}\n"
+        )
+        out_filepath.write_text(full_text)
+
+
+def _build_ppm_header(width: int, height: int, identifier: str = "P3", maxval: int = 255) -> str:
+    """
+    Build a Portable Pixmap (PPM) header from the provided parameters.
+
+    The PPM header is assumed to contain 3 lines:
+        1. Identifier, or "magic" number
+        2. Image width and height
+        3. Maximum color value
+    """
+    header = f"{identifier}\n{width} {height}\n{maxval}"
+    return header
+
+
+def _pixels_to_ppm(pixels: np.ndarray, maxval: int = 255, maxlen: int | None = 70) -> str:
+    """
+    Convert the provided pixel array to PPM format.
+
+    The pixel array is assumed to be a numeric `NxMx3` array representing pixel RGB values. Color
+    values are assumed to be between `0` and `1`, inclusive, and are scaled to integers by `maxval`.
+    Any scaled values less than `0` or greater than `maxval` are clamped. Pixel values are then
+    unwrapped row-wise into an (N*3)xM array of pixel values.
+
+    If `maxlen` is not `None`, an attempt is made to limit each data row to a maximum length of
+    `maxlen` characters.
+    """
+    # Scale to maxval and clamp
+    scaled = (pixels * maxval).astype(int)
+    scaled[scaled > maxval] = maxval
+    scaled[scaled < 0] = 0
+
+    # Now unwrap into the pixel rows
+    _, height, *_ = pixels.shape
+    scaled = scaled.reshape([height, -1])
+
+    # There's probably a way to get numpy to do what we want but this is fine
+    # Temporarily remove numpy's print setting since we're manually delimiting
+    with np.printoptions(linewidth=np.inf):  # type: ignore[arg-type]
+        tmp = np.array2string(scaled)
+        tmp = "\n".join(" ".join(row.strip("[] ").split()) for row in tmp.splitlines())
+
+    if maxlen is not None:
+        return textwrap.fill(tmp, width=maxlen)
+    else:
+        return tmp
--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+from textwrap import dedent
+
+import numpy as np
 import pytest
 
 from ray_tracer.canvas import Canvas, _build_ppm_header, _pixels_to_ppm
@@ -32,3 +36,60 @@ def test_non_color_write_raises() -> None:
     c = Canvas(10, 20)
     with pytest.raises(ValueError):
         c.write_pixel(2, 3, point(1, 2, 3))
+
+
+def test_build_ppm_header() -> None:
+    assert _build_ppm_header(5, 3) == "P3\n5 3\n255"
+    assert _build_ppm_header(5, 3, "Hello", 20) == "Hello\n5 3\n20"
+
+
+def test_array_to_ppm_no_wrap() -> None:
+    a = np.zeros(shape=(5, 3, 3))
+    a[0, 0, :] = (1.5, 0, 0)
+    a[2, 1, :] = (0, 0.5, 0)
+    a[4, 2, :] = (-0.5, 0, 1)
+
+    truth = dedent(
+        """\
+        255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 127 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"""
+    )
+    assert _pixels_to_ppm(a, maxlen=None) == truth
+
+
+def test_array_to_ppm_wrapped() -> None:
+    a = np.zeros(shape=(5, 3, 3))
+    a[0, 0, :] = (1.5, 0, 0)
+    a[2, 1, :] = (0, 0.5, 0)
+    a[4, 2, :] = (-0.5, 0, 1)
+
+    truth = dedent(
+        """\
+        255 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 127 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 255"""
+    )
+    assert _pixels_to_ppm(a, maxlen=42) == truth
+
+
+def test_ppm_write(tmp_path: Path) -> None:
+    c = Canvas(5, 3)
+    c.write_pixel(0, 0, color(1.5, 0, 0))
+    c.write_pixel(2, 1, color(0, 0.5, 0))
+    c.write_pixel(4, 2, color(-0.5, 0, 1))
+
+    out_img = tmp_path / "my_img.ppm"
+    c.to_ppm(out_img)
+
+    # The trailing newline is intentional
+    truth = dedent(
+        """\
+        P3
+        5 3
+        255
+        255 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 255
+        """
+    )
+    assert out_img.read_text() == truth
