9136d570abc88b5b9e5e392d9b6db0b4b7d5993a
S. Co1 <sco1.git@gmail.com>
2023-03-19 21:15:27 -0400

    Refactor shape objects to allow for easier addition of new shapes

--- ray_tracer/shapes.py
+++ ray_tracer/shapes.py
@@ -1,5 +1,4 @@
 import math
-from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 
 from ray_tracer.intersections import Intersection, Intersections
@@ -9,17 +8,59 @@ from ray_tracer.rays import Ray
 from ray_tracer.transforms import Matrix
 
 
-class ShapeBase(ABC):  # noqa: D101  # pragma: no cover
-    transform: Matrix
-    material: Material
+@dataclass(frozen=True, slots=True, eq=False)
+class ShapeBase:
+    """
+    Base class for creating shape objects; this is not intended to be instantiated.
 
-    @abstractmethod
-    def intersect(self, ray: Ray) -> Intersections:  # noqa: D102
-        ...
+    Child instances must define `_local_intersect` and `_local_normal_at` to calculate their
+    respective local values, which are then transformed into world coordinates by the base methods.
+    """
 
-    @abstractmethod
-    def normal_at(self, query: Rayple) -> Rayple:  # noqa: D102
-        ...
+    transform: Matrix = field(default_factory=Matrix.identity)
+    material: Material = Material()
+
+    def _local_intersect(self, local_ray: Ray) -> Intersections:  # pragma: no cover
+        raise NotImplementedError
+
+    def intersect(self, ray: Ray) -> Intersections:
+        """
+        Calculate the time position(s) where the provided Ray intersects the shape.
+
+        NOTE: To assist with object overlaps, if the ray is tangent to the shape, the time position
+        is returned twice.
+
+        NOTE: Negative timesteps are considered, so if the ray originates inside or beyond the
+        shape then negative value(s) can be returned.
+        """
+        # Apply the inverse of the shape's transformation to the ray to account for the desired
+        # shape transformation
+        transformed_ray = ray.transform(self.transform.inv())
+        return self._local_intersect(transformed_ray)
+
+    def _local_normal_at(self, local_point: Rayple) -> Rayple:  # pragma: no cover
+        raise NotImplementedError
+
+    def normal_at(self, query: Rayple) -> Rayple:
+        """Calculate the normal vector from the shape at the provided surface point."""
+        if query.w != RaypleType.POINT:
+            raise ValueError("Query location must be a point.")
+
+        # To account for transformations, we have to shift the query point from world space to the
+        # object space, otherwise our initial normal is not going to be accurate
+        local_point = self.transform.inv() * query
+        local_normal = self._local_normal_at(local_point)
+
+        # Once we've shifted to object space to get the object normal, we need to shift this back to
+        # the world space by transforming it with the inverse transpose of the sphere's
+        # transformation matrix
+        # Technically, we should be finding the 3x3 submatrix of the transform in order to prevent
+        # any translation from messing with the rayple type; to get around this, we can just make a
+        # new vector from the components
+        world_normal = self.transform.inv().transpose() * local_normal
+        world_normal = vector(*world_normal)
+
+        return world_normal.normalize()
 
 
 @dataclass(frozen=True, slots=True, eq=False)
@@ -33,23 +74,8 @@ class Sphere(ShapeBase):
     NOTE: Spheres are compared by object ID only, so no 2 instances will compare `True`
     """
 
-    transform: Matrix = field(default_factory=Matrix.identity)
-    material: Material = Material()
-
-    def intersect(self, ray: Ray) -> Intersections:
-        """
-        Calculate the time position(s) where the provided Ray intersects the `Sphere`.
-
-        NOTE: To assist with object overlaps, if the ray is tangent to the sphere, the time position
-        is returned twice.
-
-        NOTE: Negative timesteps are considered, so if the ray originates inside or beyond the
-        sphere then negative value(s) can be returned.
-        """
+    def _local_intersect(self, transformed_ray: Ray) -> Intersections:
         # Calculate the discriminant to determine if there are any intersections
-        # Apply the inverse of the sphere's transformation to the ray to account for the desired
-        # sphere transformation
-        transformed_ray = ray.transform(self.transform.inv())
         sphere_to_ray = transformed_ray.origin - point(0, 0, 0)
         a = dot(transformed_ray.direction, transformed_ray.direction)
         b = 2 * dot(transformed_ray.direction, sphere_to_ray)
@@ -66,23 +92,5 @@ class Sphere(ShapeBase):
 
         return Intersections(intersections)
 
-    def normal_at(self, query: Rayple) -> Rayple:
-        """Calculate the normal vector from the `Sphere` at the provided surface point."""
-        if query.w != RaypleType.POINT:
-            raise ValueError("Query location must be a point.")
-
-        # To account for transformations, we have to shift the query point from world space to the
-        # object space, otherwise our initial normal is not going to be accurate
-        object_point = self.transform.inv() * query
-        object_normal = object_point - point(0, 0, 0)
-
-        # Once we've shifted to object space to get the object normal, we need to shift this back to
-        # the world space by transforming it with the inverse transpose of the sphere's
-        # transformation matrix
-        # Technically, we should be finding the 3x3 submatrix of the transform in order to prevent
-        # any translation from messing with the rayple type; to get around this, we can just make a
-        # new vector from the components
-        world_normal = self.transform.inv().transpose() * object_normal
-        world_normal = vector(*world_normal)
-
-        return world_normal.normalize()
+    def _local_normal_at(self, local_point: Rayple) -> Rayple:
+        return local_point - point(0, 0, 0)
